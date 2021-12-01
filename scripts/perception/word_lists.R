e497dc83d3cf79e00df2f4fc0798b3aa139e03a0
kparrish92 <60477545+kparrish92@users.noreply.github.com>
2021-11-30 20:32:18 -0500

    updates

---
+++ scripts/perception/word_lists.R
@@ -0,0 +1,106 @@
+
+
+
+stimuli_shadowing_en <- c("pop", "peep", "pool",
+                       "cop", "keep", "cool", 
+                       "top", "teak", "tool" 
+                       ) 
+
+
+stimuli_shadowing_sp <- c("pato", "piso", "puro",
+                          "cama", "quiso", "cura",
+                          "talla", "tiro", "tubo"
+                       
+) 
+
+
+stimuli_shadowing_fr <- c("patte", "pile", "poudre",
+                          "canne", "quitte", "cuve", 
+                          "tasse", "tir", "tout"
+) 
+
+stimuli_shadowing_ge <- c("Pampe", "Piel", "Punkt", 
+                          "kann", "Kiel", "kuhn", 
+                          "Tante", "tief", "Tulpe"
+) 
+
+
+# eng conditions
+# pi - p^ 
+# ki - k^
+# ti - t^
+
+# span conditions
+# pi - po
+# ki - ko
+# ti - to
+
+# german conditions
+# pi - po - p^
+# ki - ko - k^
+# ti - to - t^
+
+# Production tasks
+## Shadowing task vowels and stops
+## Word reading task vowels and stops
+
+# Perceptions tasks
+## AX stop cont p/b continuum 
+## AX vowel cont pat-pot continuum, 
+## Stop phoneme cat task p/b continuum given two choices (afc with ratings)
+## vowel phoneme cat task 4 vowel conditions
+
+
+ 
+# eng 
+sulk
+such
+sun
+
+# span
+
+son
+sol
+
+# french 
+
+forer
+faut 
+feder
+fe
+
+# german 
+o 
+e
+
+# 4 conditions in production
+
+# short/i/
+# both /i/ borrow from vots  
+# sp /o/ elicit in fricative context 
+# eng /^/ 
+# neither /y/ 
+
+
+## add /o/ to span with stops, /^/ to eng with stops, both /o/ and /^/ to french and german, even if the words are made up
+
+
+
+
+####
+# Same participants - French and German 
+# 
+#  /ɪ	- iː/ from German (AX) German contrast (eng only)
+#  /ɛ - e/ from french (AX) French contrast (eng only)
+
+# 8x conditions 
+#  /ɛ/ 
+#  /e/
+#  /ɪ/
+# /iː/
+# pot
+# spanish /a/ 
+# /y/ from french (not in either language)
+# /o/ (span only)
+# German /p/
+# french /p/ 
